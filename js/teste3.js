bf3724473b6de1a7c777fa7b1c932775ac393e25
Amanda Rodrigues <122230863+ymandy1@users.noreply.github.com>
2023-07-03 21:48:35 -0300

    javascript contact.js

--- js/teste3.js
+++ js/teste3.js
@@ -9,3 +9,34 @@ icon.onclick = function() {
         icon.src = "..//image/moon.png";
     }
 }
+
+const contactForm = document.getElementById('contact-form');
+const messageContainer = document.getElementById('message-container');
+
+contactForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const formData = new FormData(contactForm);
+
+  fetch(contactForm.action, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json'
+    },
+    body: formData
+  })
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Erro ao enviar o formulário.');
+      }
+    })
+    .then(function (data) {
+      messageContainer.innerHTML = '<span class="success">Mensagem enviada com sucesso!</span>';
+      contactForm.reset();
+    })
+    .catch(function (error) {
+      console.error(error);
+      messageContainer.innerHTML = '<span class="error">Não foi possível enviar sua mensagem. Tente novamente mais tarde.</span>';
+    });
+});
